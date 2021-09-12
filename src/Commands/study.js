2b9e240d77184a31f7d4868e048ca9051a5b4280
AKS <60446729+AngKS@users.noreply.github.com>
2021-09-12 22:34:35 +0800

    Added buttons to the study thread embed. will take a break for a week before continuing

--- src/Commands/study.js
+++ src/Commands/study.js
@@ -1,34 +1,61 @@
-const {Intents, MessageAttachment, MessageActionRow, MessageButton, MessageEmbed } = require('discord.js');
+const { Intents, MessageAttachment, MessageActionRow, MessageButton, MessageEmbed } = require('discord.js');
 const Command = require("../structures/Command")
 
 
 module.exports = new Command({
-    name : "study",
-    description : "AKS Bot study thread",
-    type : "SLASH",
-    slashCommandOptions : [],
-    run : async (message, args, client) => {
+    name: "study",
+    description: "AKS Bot study thread",
+    type: "SLASH",
+    slashCommandOptions: [],
+    run: async (message, args, client) => {
+
+        // Creating the personal Study Thread
         const studyThread = await message.channel.threads.create({
-            name : `${message.user.username}'s Study thread!`,
+            name: `${message.user.username}'s Study thread!`,
             autoArchiveDuration: 60,
             reason: `${message.user.username} needs to study!`
         }).then(async (thread) => {
             thread.members.add(message.user.id)
 
+            // Welcome embed buttons
+            let welcomeRow = new MessageActionRow()
+                .addComponents(
+                    new MessageButton()
+                        .setLabel("Pomodoro")
+                        .setStyle("DANGER")
+                        .setCustomId("pomo")
+                )
+                .addComponents(
+                    new MessageButton()
+                        .setLabel("TO-DO List")
+                        .setStyle("LINK")
+                        .setURL("https://www.github.com/angks/aks")
+                )
+                .addComponents(
+                    new MessageButton()
+                        .setLabel("Flash Card")
+                        .setStyle("PRIMARY")
+                        .setCustomId("fc")
+                )
+
+            // Welcome Embed
             let welcomeEmbed = new MessageEmbed()
                 .setTitle(`Hey **${message.user.username}**, This thread is created specifically to help you study!`)
-                .setThumbnail(message.user.avatarURL({dynamic : true}))
+                .setThumbnail(message.user.avatarURL({ dynamic: true }))
                 .setColor("#fffff")
                 .setDescription("These are the different Commands you can use in this thread.")
                 .setFields(
-                    {name: "Pomodoro", value: "Pomodoro timer to keep you focused while farming some koins!"},
-                    {name: "TODO", value: "Your personal To-Do List!"},
-                    {name: "Flashcard", value: "Quiz yourself with custom flashcards!"},
+                    { name: "Pomodoro", value: "Pomodoro timer to keep you focused while farming some koins!" },
+                    { name: "TODO", value: "Your personal To-Do List!" },
+                    { name: "Flashcard", value: "Quiz yourself with custom flashcards!" },
 
                 )
                 .setTimestamp()
-            
-            thread.send({embeds: [welcomeEmbed]})
+
+
+
+
+            thread.send({ embeds: [welcomeEmbed], components: [welcomeRow] })
         })
 
 
