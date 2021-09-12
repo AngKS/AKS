0d53f063fb2476c11ed33c0d65596ce9344d5dcb
AKS <60446729+AngKS@users.noreply.github.com>
2021-09-12 16:36:34 +0800

    deleted congratulations command as it was causing errors

--- src/Commands/help.js
+++ src/Commands/help.js
@@ -5,13 +5,8 @@ const Command = require("../structures/Command")
 module.exports = new Command({
     name: "help",
     description: "AKS-Bot's Help Menu",
-    type: "SLASH",
-    slashCommandOptions: [{
-        name: "command",
-        description: "Input command name for command specific help menu!",
-        type: "STRING",
-        required: false
-    }],
+    type: "BOTH",
+    slashCommandOptions: [],
     permissions: "SEND_MESSAGES",
     run: async (message, args, client) => {
         const row = new MessageActionRow()
@@ -29,12 +24,12 @@ module.exports = new Command({
             )
         console.log(client.guild)
 
-
         const embed = new MessageEmbed()
             .setColor('#0099ff')
             .setTitle(`AKS Bot's Help Menu`)
             .setURL('https://discord.js.org')
             .setDescription('Some description here');
         await message.reply({ embeds: [embed], components: [row] });
+
     }
 })
