169e0d9801706f20a7ac6217843b3c33dddf7e44
AKS <60446729+AngKS@users.noreply.github.com>
2021-09-12 14:55:11 +0800

    added prune command

---
+++ src/Commands/prune.js
@@ -0,0 +1,33 @@
+
+const Command = require("../structures/Command")
+const Discord = require("discord.js")
+
+module.exports = new Command({
+    name: "prune",
+    description: "Use this command to clear n-number of message(s)",
+    type: "BOTH",
+    slashCommandOptions: [{
+        name: 'amount',
+        description: "The amount of messages to be cleared",
+        type: "INTEGER",
+        required: true
+    }],
+    permissions: "MANAGE_MESSAGES",
+    run: async (message, args, client) => {
+        const amount = args[1]
+
+        if (!amount || isNaN(amount)) return message.reply(`${amount === undefined ? "Wow you don't even know numbers?" : "I guess you don't know how to fucking count!"}`)
+
+        const amountParsed = parseInt(amount)
+
+        if (amountParsed > 100) return message.reply({ content: `My dude, there's only so much i can do... *max. 100 messages*` })
+
+        message.channel.bulkDelete(amountParsed)
+
+        const msg = await message.send(`Successfully cleared ${amountParsed} messages!`)
+
+        if (msg) setTimeout(() => { msg.delete() }, 5000)
+
+    }
+
+})
