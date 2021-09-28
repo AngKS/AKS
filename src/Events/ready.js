045e4254dc21286eab5414cae497065d20426c4d
AKS <60446729+AngKS@users.noreply.github.com>
2021-09-28 15:44:57 +0800

    minor fix for Bot Startup

--- src/Events/ready.js
+++ src/Events/ready.js
@@ -1,6 +1,6 @@
 const Event = require("../structures/Event")
 
 module.exports = new Event("ready", (client) => {
-    console.log(`${client.user} is now online`)
+    console.log(`${client.user.username} is now online`)
 
 })
