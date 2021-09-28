ccba775037ae2107c15fac01dbe0a663119c114c
AKS <60446729+AngKS@users.noreply.github.com>
2021-09-28 20:58:09 +0800

    work in progress on TODO command

--- src/Commands/todo.js
+++ src/Commands/todo.js
@@ -1,125 +1,108 @@
 const { MessageActionRow, MessageButton, MessageEmbed } = require('discord.js')
 const Command = require("../structures/Command")
-const {mongo} = require("../data/db.json")
+const { mongo } = require("../data/db.json")
 const MongoClient = require('mongodb').MongoClient;
 const clipboardy = require("clipboardy")
 const { time } = require('@discordjs/builders')
 
-let getTasks = async(username) => {
-    MongoClient.connect(mongo).then(client =>{
+let getTasks = async (username) => {
+    MongoClient.connect(mongo).then(client => {
         console.log('Database Connected!')
         const db = client.db("AKS_Bot")
-        const collection = db.collection("users")
+        const collection = db.collection("tasks")
 
         collection.find().toArray().then(results => {
-            let user = results.find(obj => obj.name === username)
+            let tasks = results.find(obj => obj.user === username)
 
-            if (user){
-                return user
+            if (tasks) {
+                let allTasks = []
+                allTasks.push(tasks)
+                return allTasks
             }
-            else{
+            else {
                 return null
             }
         })
-        
+
     })
 }
 
-let addTask = async(username, task) => {
+let addTask = async (task) => {
     MongoClient.connect(mongo).then(client => {
         console.log('Database Connected')
         const db = client.db("AKS_Bot")
-        const collection = db.collection("users")
+        const collection = db.collection("tasks")
 
-        tasks.find().toArray().then(results => {
-            let user = results.find(obj => obj.name === username)
-
-            if (user) {
-                collection.findOneAndUpdate(
-                    {name : username},
-                    {
-                        $set : {
-                            tasks : user.tasks.append(task)
-                        }
-                    },
-                    {
-                        upsert: true
-                    }
-                ).then(result => console.log(result)
-                ).catch(err => console.log(err)
-                )
+        collection.find().toArray().then(results => {
+            let tasks = results.find(obj => obj.name === task.name)
+            if (tasks) {
+                return "Tasks already exists!"
             }
             else {
-                collection.insertOne({
-                    name: username,
-                    level: 1,
-                    koins: 0,
-                    items: [],
-                    tasks: user.tasks.append(task)
-                }).then(result => {
+                collection.insertOne(task).then(result => {
                     console.log(result)
                     client.close()
-                    
+                    return "Tasks successfully added!"
+
                 }).catch(err => console.log(err))
             }
         })
 
-        
+
     })
 }
 
 module.exports = new Command({
-    name : "todo",
+    name: "todo",
     description: "Your personal To-Do list in discord",
     type: "SLASH",
     slashCommandOptions: [{
-        name : "subcomand",
+        name: "subcomand",
         description: "list, add, remove, complete",
         type: "STRING",
         required: true
-    },{
+    }, {
         name: "condition",
         description: "all, 'item name', index",
         type: "STRING",
         required: true
     }],
     permissions: "SEND_MESSAGES",
-    run : async (message, args, client) => {
+    run: async (message, args, client) => {
         let subCommand = args[1].toLowerCase()
         let condition = args[2].toLowerCase()
 
 
-        let data = {
-            001 : {
-                task: "Task 01",
-                status: "COMPLETED",
-                created_at: new Date().toLocaleDateString()
-            },
-            002 : {
-                task: "Task 02",
-                status: "INCOMPLETE",
-                created_at: new Date().toLocaleDateString()
-            }
-        }
 
         let subCommands = ['list', 'add', 'remove', 'complete']
 
-        if (!subCommands.includes(subCommand)) return message.reply({content: "Please specify the correct parameters"})
-        
+        if (!subCommands.includes(subCommand)) return message.reply({ content: "Please specify the correct parameters" })
+
         switch (subCommand) {
             case "list":
                 // for(const [key, value] of Object.entries(data)){
                 //     console.log(key, value)
-                    
-                // }
 
-                console.log(await getTasks(message.user.username));
-                break;
-        
+                // }
+                let taskList = await getTasks(message.user.username)
+                
+                await getTasks(message.user.username).then(taskLists => message.channel.send({ content: taskLists === undefined ? "No result" : taskLists }))
+            case "add":
+
+                let newTask = {
+                    name: "Task01",
+                    user: message.user.username,
+                    description: "lorem ipsum",
+                    created_at: new Date().toLocaleDateString()
+                }
+
+                await addTask(newTask).then(addMsg => message.channel.send({ content: addMsg === undefined ? "No result" : addMsg }))
+                
+
             default:
                 break;
         }
-        
+
 
     }
 })
